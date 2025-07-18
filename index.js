dbf545d07ecdff32a908937dc82882d57be18ad4
Adekoya-Tech <Adekoyapeter2004@gmail.com>
2025-07-18 02:31:43 +0100

    Innitial commit

--- index.js
+++ index.js
@@ -5,4 +5,27 @@ function toggleMenu() {
   icon.classList.toggle("open");
 
 
-}
+};
+
+
+const btn = document.getElementById("button");
+
+document.getElementById("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  btn.value = "Sending...";
+
+  const serviceID = "default_service";
+  const templateID = "template_ehfnl1y";
+
+  emailjs.sendForm(serviceID, templateID, this).then(
+    () => {
+      btn.value = "Send Email";
+      alert("Sent!");
+    },
+    (err) => {
+      btn.value = "Send Email";
+      alert(JSON.stringify(err));
+    }
+  );
+});
